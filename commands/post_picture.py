57893518efc7eb1ea64b6f9dde486fe3f000a4b4
infi <infiziert@protonmail.ch>
2019-04-26 11:06:25 +0200

    update files

--- commands/post_picture.py
+++ commands/post_picture.py
@@ -44,17 +44,17 @@ async def yaoi(client, message, args):
     await delete_user_message(message)
     await send_picture(client, yaoi_folder, 328942447784624128)
 
-
-@register_command('paid', description='Post the getting_paid picture.')
-async def paid(client, message, args):
+# Spam Picture
+@register_command('autism', description='Autism!')
+async def autism(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "getting_paid.jpg"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "autism.gif"), content=f"From: {message.author.display_name}")
 
 
-@register_command('masterrace', description='Arch == Masterrace.')
-async def masterrace(client, message, args):
+@register_command('bait', description='Bait (Arch Linux)')
+async def bait(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "arch.png"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "bait.png"), content=f"From: {message.author.display_name}")
 
 
 @register_command('bestunion', description='sowjetunion == bestunion.')
@@ -63,16 +63,34 @@ async def bestunion(client, message, args):
     await message.channel.send(file=discord.File(spam_folder + "sowjet.png"), content=f"From: {message.author.display_name}")
 
 
-@register_command('linux', description='Linux.')
-async def linux(client, message, args):
+@register_command('capi', description='Capitalism')
+async def capi(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "linux.png"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "capi.jpg"), content=f"From: {message.author.display_name}")
 
 
-@register_command('yuriislove', description='Yuri is love.')
-async def yuriislove(client, message, args):
+@register_command('cute', description='Waifu trash')
+async def cute(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "yuri.jpeg"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "cute.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('eyes', description='Waifu trash')
+async def eyes(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "eyes.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('jail', description='Jail is over there!')
+async def jail(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "jail.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('kage', description='The normal kage')
+async def kage(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "kage.png"), content=f"From: {message.author.display_name}")
 
 
 @register_command('kevin', description='Kevin! Aus!')
@@ -81,19 +99,49 @@ async def kevin(client, message, args):
     await message.channel.send(file=discord.File(spam_folder + "kevin.jpeg"), content=f"From: {message.author.display_name}")
 
 
-@register_command('capi', description='Capitalism')
-async def capi(client, message, args):
+@register_command('linux', description='Linux.')
+async def linux(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "capi.jpg"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "linux.png"), content=f"From: {message.author.display_name}")
 
 
-@register_command('autism', description='Autism!')
-async def autism(client, message, args):
+@register_command('masterrace', description='Arch == Masterrace.')
+async def masterrace(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "autism.gif"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "arch.png"), content=f"From: {message.author.display_name}")
 
 
-@register_command('jail', description='Jail is over there!')
-async def jail(client, message, args):
+@register_command('paid', description='Post the getting_paid picture.')
+async def paid(client, message, args):
     await delete_user_message(message)
-    await message.channel.send(file=discord.File(spam_folder + "jail.gif"), content=f"From: {message.author.display_name}")
+    await message.channel.send(file=discord.File(spam_folder + "getting_paid.jpg"), content=f"From: {message.author.display_name}")
+
+
+@register_command('season2', description='Waifu trash')
+async def season2(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "season2.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('trash', description='Anime is trash')
+async def trash(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "trash.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('waifu', description='Waifu trash')
+async def waifu(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "waifu.gif"), content=f"From: {message.author.display_name}")
+
+
+@register_command('windows', description='Windows trash')
+async def windows(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "windows.jpg"), content=f"From: {message.author.display_name}")
+
+
+@register_command('yuri_is_love', description='Yuri is love.')
+async def yuri_is_love(client, message, args):
+    await delete_user_message(message)
+    await message.channel.send(file=discord.File(spam_folder + "yuri.jpeg"), content=f"From: {message.author.display_name}")
