4048120ca30bffee6ae320fc4c0fa5861bd65139
FichteFoll <fichtefoll2@googlemail.com>
2021-04-05 17:26:17 +0200

    Explicitly prevent everyone mentions
    I hadn't considered that this could be used to elevate mention power to
    the bot's permissions, so we prevent everyone mentions explicitly.

--- modules/timer.py
+++ modules/timer.py
@@ -59,6 +59,7 @@ def parse_datetime(time_string: str) -> Optional[datetime]:
 ########################################
 
 NO_MENTIONS = discord.AllowedMentions(everyone=False, users=False, roles=False, replied_user=False)
+NO_EVERYONE_MENTIONS = discord.AllowedMentions(everyone=False)
 MAX_DELTA = timedelta(days=31)
 
 data: Dict[str, Any] = {}
@@ -116,8 +117,14 @@ async def send_reminder(client, timer: Timer):
         channel_id=timer.channel_id,
         guild_id=timer.guild_id,
     )
-    await channel.send(text, reference=reference, mention_author=True)
-    # TODO what about failure? schedule retry? retry on next startup? how often?
+    await channel.send(
+        text,
+        reference=reference,
+        # Generally we want to allow mentions,
+        # but no power abuse through the bot's permissions
+        allowed_mentions=NO_EVERYONE_MENTIONS,
+        mention_author=True,
+    )
 
 
 def schedule(client, timer):
